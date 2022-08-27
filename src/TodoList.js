b0b898792e98e980e2f02c0127a77913212303d5
kundalili <105823291+kundalili@users.noreply.github.com>
2022-08-27 16:54:29 +0200

    todo-list

---
+++ src/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+export default function TodoList() {
+  return (
+    <div className="App">
+        <nav className="nav-bar"> 
+            <form id="search">
+            <input className="searchTask" type="text" placeholder=""></input>
+            </form> 
+        </nav>
+    </div>
+  )
+}
+
