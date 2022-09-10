4fff0c7ff44f3f1ee8f3cd58299c3810315e3f06
SadafShahid20 <107463181+SadafShahid20@users.noreply.github.com>
2022-09-10 12:59:55 +0200

    Add files via upload
    sadaf

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Screen from './screen'
+import Cards from './ReactApp/reactapp'
+
+function App() {
+  return (
+  <>
+  <Screen/>
+  <Cards />
+  </>
+  
+  )
+
+  };
+  export default App;
---
+++ screen.js
@@ -0,0 +1,15 @@
+import Date from './Welcome/date'
+import {FaTwitter} from "react-icons/fa"
+
+
+export default function Screen(){
+    return (
+        <div className="screen">
+          <Date/>
+
+        </div>
+    )
+    
+
+}
+console.log(Screen());
